5233ce6cb94229589f8ac10162c5e355b7e6eb0c
Luis Cruz <luis.crz.v@gmail.com>
2021-06-05 10:52:33 -0500

    Update index and db

---
+++ db/seed.sql
@@ -0,0 +1,36 @@
+use employees;
+--Insert values to the department table"
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+--Insert values to the role table"
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson',80000, 1),
+    ('Lead Engineer',150000, 2),
+    ('Software Engineer',120000, 2),
+    ('Account Manager',160000, 3),
+    ('Accountant',125000, 3),
+    ('Legal Team Lead',250000, 4),
+    ('Lawyer',190000, 4);  
+
+--Insert values to the employee table"
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),    
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7)
