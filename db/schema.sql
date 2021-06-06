cb0c22eaa18c7c985783106078546e84917a4de1
Luis Cruz <luis.crz.v@gmail.com>
2021-06-05 21:23:50 -0500

    Update Database

--- db/schema.sql
+++ db/schema.sql
@@ -24,8 +24,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
     INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT UNSIGNED,
-    INDEX man_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    INDEX man_ind (manager_id)
 );
